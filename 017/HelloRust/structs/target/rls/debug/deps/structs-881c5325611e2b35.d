076ba481495006cf56ac8663ebe44c9f116512fa
Hamza Muric <hamzamuric9@gmail.com>
2019-04-24 23:56:43 +0200

    structs

--- 017/HelloRust/structs/target/rls/debug/deps/structs-881c5325611e2b35.d
+++
@@ -1,5 +0,0 @@
-/home/hamza/structs/target/rls/debug/deps/structs-881c5325611e2b35.rmeta: src/main.rs
-
-/home/hamza/structs/target/rls/debug/deps/structs-881c5325611e2b35.d: src/main.rs
-
-src/main.rs:
--- 017/HelloRust/structs/target/rls/debug/deps/structs-da2e49098bd3dbcd.d
+++
@@ -1,5 +0,0 @@
-/home/hamza/structs/target/rls/debug/deps/structs-da2e49098bd3dbcd.rmeta: src/main.rs
-
-/home/hamza/structs/target/rls/debug/deps/structs-da2e49098bd3dbcd.d: src/main.rs
-
-src/main.rs:
