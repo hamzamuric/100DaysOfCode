dbecc522624b2f8078d838c3fff7fe6a5328a17d
Hamza Muric <hamzamuric9@gmail.com>
2019-05-09 21:34:21 +0200

    fractal

---
+++ 017/HelloRust/fractal/src/main.rs
@@ -0,0 +1,43 @@
+extern crate image;
+extern crate num_complex;
+
+use image::{ImageBuffer, Rgb};
+use num_complex::Complex;
+
+fn main() {
+    let imgx = 800;
+    let imgy = 800;
+
+    let scalex = 3.0 / imgx as f32;
+    let scaley = 3.0 / imgy as f32;
+
+    let mut imgbuf = ImageBuffer::new(imgx, imgy);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = Rgb([r, 0, b]);
+    }
+
+    for x in 0..imgx {
+        for y in 0..imgy {
+            let cx = y as f32 * scalex - 1.5;
+            let cy = x as f32 * scaley - 1.5;
+
+            let c = Complex::new(-0.4, 0.6);
+            let mut z = Complex::new(cx, cy);
+
+            let mut i = 0;
+            while i < 255 && z.norm() <= 2.0 {
+                z = z * z + c;
+                i += 1;
+            }
+
+            let pixel = imgbuf.get_pixel_mut(x, y);
+            let data = (*pixel as Rgb<u8>).data;
+            *pixel = Rgb([data[0], i as u8, data[2]]);
+        }
+    }
+
+    imgbuf.save("fractal.png").unwrap();
+}
